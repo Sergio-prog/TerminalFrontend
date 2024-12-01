08dd9847df191982206b62d8a431e05fa6ac3b2d
mashk4 <99094113+mashk4@users.noreply.github.com>
2024-12-01 23:46:20 +0200

    Fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,4 @@
-// const API_BASE_URL = 'https://secret-ocean-19070-7d15bdda8dde.herokuapp.com/api';
-const API_BASE_URL = 'http://0.0.0.0:8000/api';
+const API_BASE_URL = 'https://secret-ocean-19070-7d15bdda8dde.herokuapp.com/api';
 export type TimeRange = 'm5' | 'h1' | 'h6' | 'h24';
 
 export interface NewPair {
