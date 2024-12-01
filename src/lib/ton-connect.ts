16bc31adc432a35a7442e1863adadf90ec3e6f27
Sergey Nesterov <sergioreklamplay@gmail.com>
2024-12-02 00:33:00 +0200

    Add config for tonconnect

--- src/lib/ton-connect.ts
+++ src/lib/ton-connect.ts
@@ -1,7 +1,7 @@
 import { TonConnectOptions } from '@tonconnect/ui-react';
 
 export const tonConnectOptions: TonConnectOptions = {
-    manifestUrl: 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json',
+    manifestUrl: 'https://raw.githubusercontent.com/Sergio-prog/TerminalFrontend/refs/heads/main/public/tonconnect-manifest.json',
 };
 
 export const CHAIN = '-239';
