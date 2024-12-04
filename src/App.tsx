06da402815d44886d3f8477b0af1b337f09a7a1d
mashk4 <99094113+mashk4@users.noreply.github.com>
2024-12-05 01:09:39 +0200

    Add GA

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
-import TelegramMiniApp from './pages/TelegramMiniApp'
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import TelegramMiniApp from './pages/TelegramMiniApp';
 
-function App() {
+const App = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        if (typeof window.gtag === 'function') {
+            window.gtag('config', 'G-2J1EHL03SV', {
+                page_path: location.pathname,
+            });
+        }
+    }, [location]);
+
     return (
         <Routes>
             <Route path="/" element={<TelegramMiniApp />} />
         </Routes>
-    )
-}
+    );
+};
 
-export default App  
+export default App;
